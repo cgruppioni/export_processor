9d11ed73dfb54d27d5cbb3fd5a4bfecfae48a2be
Casey Gruppioni <cgruppioni@gmail.com>
2019-05-15 15:54:18 -0400

    Use express server

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
-// Basic rendering of a vue instance
-// https://ssr.vuejs.org/guide/#rendering-a-vue-instance
+// Use express https://ssr.vuejs.org/guide/#integrating-with-a-server
 
-// Step 1: Create a Vue instance
 const Vue = require('vue')
-const app = new Vue({
-  template: `<div>Hello World</div>`
-})
-
-// Step 2: Create a renderer
+const server = require('express')()
 const renderer = require('vue-server-renderer').createRenderer()
 
-// Step 3: Render the Vue instance to HTML
-renderer.renderToString(app, (err, html) => {
-  if (err) throw err
-  console.log(html);
-  // => <div data-server-rendered="true">Hello World</div>
+server.get('*', (req, res) => {
+  const app = new Vue({
+    data: {
+      url: req.url
+    },
+    template: `<div>The visited URL is: {{ url }}</div>`
+  })
+
+  renderer.renderToString(app, (err, html) => {
+    if (err) {
+      res.status(500).end('Internal Server Error')
+      return
+    }
+    res.end(`
+      <!DOCTYPE html>
+      <html lang="en">
+        <head><title>Hello</title></head>
+        <body>${html}</body>
+      </html>
+    `)
+  })
 })
 
-// in 2.5.0+, returns a Promise if no callback is passed:
-renderer.renderToString(app).then(html => {
-  console.log(html);
-}).catch(err => {
-  console.error(err);
-})
+server.listen(8080)
